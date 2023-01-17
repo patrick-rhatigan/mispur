e74b629bbb0aae8d7f05ba2c1079c5d61c6bdc6f
patrick-rhatigan <prhatigan@outlook.com>
2023-01-17 15:23:31 -0500

    Corrections to Deltahatinf calculation

--- R/spur_general.R
+++ R/spur_general.R
@@ -2,17 +2,22 @@
 
 spur_general <- function(W, m, Theta, theta0s, num_inti_val, alpha,
              alpha2, S, B, sdS, iota_sd, iota_q) {
-
+#r and n defined as nrows and ncols in parent function spur
 
   #Deltahatinf calculation:
   Deltahat_fun <- gen_Deltahat_fun_g(m=m)
-  Deltahatinf <- rep(NA, ncol(Theta))
+  Deltahat <- rep(NA, ncol(Theta))
   for(i in 1:ncol(Theta)) {
     Deltahatinf[i] = optimize(Deltahat_fun, W=W,
                               lower = Theta[1,i],
-                              upper = Theta[2,i])$objective
+                              upper = Theta[2,i])
   }
-
+  Deltahat_values <- sapply(1:length(Deltahat),
+                            function(j) Deltahat[[j]]$obj,
+                            simplify = TRUE)
+  min_ind <- which(min(Deltahat_values) == Deltahat_values)[1]
+  Deltahatinf <- Deltahat_values[[min_ind]]
+  Deltahatinf_par <- Deltahat[[min_ind]]$min[-1]#?
   rhatinf <- pos(Deltahatinf)
 
 #Omegahatplus?
@@ -21,7 +26,12 @@ spur_general <- function(W, m, Theta, theta0s, num_inti_val, alpha,
   set.seed(1001)
   b_index <- matrix(sample.int(n, size = n*B, replace = TRUE), nrow = B)
 
+  #defining the Astarinf objective function:
+  Astarinf_obj <- gen_Astarinf_obj_g(W, m, n, kappa, rhatinf, iota_sd)
+
   for(b in 1:B) {
     Wstar <- W[b_index[b,],] #bootstrap sample data
+
+
   }
 }
